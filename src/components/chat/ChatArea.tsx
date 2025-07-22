73ec284366599c8abca70b892a463f3d0af46c57
Clinton James <clintjeez@gmail.com>
2025-07-22 04:46:28 +0100

    Rename project to AsklawLM

---
+++ src/components/chat/ChatArea.tsx
@@ -0,0 +1,5 @@
+@@ .. @@
+       <div className="p-4 border-t border-gray-200 flex-shrink-0">
+-        <p className="text-center text-sm text-gray-500">InsightsLM can be inaccurate; please double-check its responses.</p>
++        <p className="text-center text-sm text-gray-500">AsklawLM can be inaccurate; please double-check its responses.</p>
+       </div>
